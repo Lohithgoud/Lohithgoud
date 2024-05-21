4445e8283a6beda214a011e84df42acdd1323ef3
Lohithgoud <137982071+Lohithgoud@users.noreply.github.com>
2024-05-21 12:32:36 +0530

    leet

---
+++ python codes/Bipartite Check using dfs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dfs(self, start, col, graph, colour):
+        colour[start] = col
+        for ngbr in graph[start]:
+            if colour[ngbr] == -1:
+                if not self.dfs(ngbr, 1 - col, graph, colour):
+                    return False
+            elif (colour[ngbr] == col):
+                return False
+        return True
+
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        v = len(graph)
+        colour = [-1] * v
+        for i in range(v):
+            if colour[i] == -1:
+                if  not self.dfs(i, 0, graph, colour):
+                    return False
+
+        return True
