48675d66e5534d51772b36127dc0d36cb7c80396
Lohithgoud <137982071+Lohithgoud@users.noreply.github.com>
2024-06-08 23:04:19 +0530

    GFG,TUF,DP, TAB
     0 - 1 Knapsack Problem TAB

--- python codes/0 - 1 Knapsack Problem TAB.py
+++ python codes/0 - 1 Knapsack Problem TAB.py
@@ -1,52 +1,25 @@
 #User function Template for python3
 
 class Solution:
-    def fun(self,idx,w,wt,val,dp):
-        
-        if idx == 0:
-            if wt[0] <= w:
-                return val[0]
-            else:
-                return 0
-        
-        if dp[idx][w] != -1:
-            return dp[idx][w]
-        
-        notake = self.fun(idx - 1,w,wt,val,dp)
-        take = float("-inf")
-        
-        if wt[idx] <= w:
-            take = val[idx] + self.fun(idx-1,w - wt[idx] , wt,val,dp)
-            
-        dp[idx][w] = max(take,notake)
-        
-        return dp[idx][w]
-                
-        
-    
-    #Function to return max value that can be put in knapsack of capacity W.
+    # nction to return max value that can be put in knapsack of capacity W.
     def knapSack(self,W, wt, val, n):
         
-        dp = [[-1 for _ in range(W + 1)] for _ in range(n)]
-        return self.fun(n - 1,W,wt,val,dp)
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import atexit
-import io
-import sys
-
-# Contributed by : Nagendra Jha
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        n = int(input())
-        W = int(input())
-        val = list(map(int,input().strip().split()))
-        wt = list(map(int,input().strip().split()))
-        ob=Solution()
-        print(ob.knapSack(W,wt,val,n))
-# } Driver Code Ends
+        dp = [[0 for _ in range(W + 1)] for _ in range(n)]
+        
+        for i in range(wt[0],W + 1):
+            dp[0][i] = val[0]
+                
+        
+        
+        for idx in range(1,n):
+            for w in range(1,W + 1):
+                notake = 0 + dp[idx - 1][w]
+                
+                take = float('-inf')
+                
+                if wt[idx] <= w:
+                    take = val[idx] + dp[idx - 1][w - wt[idx]]
+                    
+                dp[idx][w] = max(take , notake)
+        
+        return dp[n - 1][W]
